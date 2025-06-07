9e5dc219be590736fc0e210a27555026cd35cee3
axiom-guy <gargaditya08072005@icloud.com>
2025-06-07 18:02:57 +0530

    Adding files for image(api and locally)

--- image_processing_local.py
+++ image_processing_local.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 import re
 import os
+from PIL import Image
 import time
-import nltk
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from alive_progress import alive_bar
-from common_functions import clean_text
 
-def generate_name_text_api(path,text,bar,client,model):
-    # summarizing
-    prompt = f"""For the given text, give me 100 words concise summary, focusing on key points.
-    Text: {text}
+
+# In[2]:
+
+
+def image_to_readable(path):
+    image=Image.open(path).convert('RGB')
+    return image
+
+
+# In[3]:
+
+
+def generate_name_image_local(path,token_image,token_text,model_image,model_text):
+    prompt = f"""For the given image, describe the image, focusing on key points.
+    Image:f'{image_to_readable(path)}'
     Summary:""" 
-    completion_summarize=client.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": f"{prompt}"
-            }
-        ],
+    inputs=token_image(prompt,return_tensors='pt').to(model_image.device)
+    outputs=model_image.generate(
+        **inputs,
+        max_new_tokens=150,
+        temperature=0.7,
+        top_p=0.85
     )
-    summary=completion_summarize.choices[0].message.content.strip()
+    summary=token_image.decode(outputs[0],skip_special_tokens=True).split("Summary:")[-1].strip()
     bar()
-    print(bar)
-
-    # filename
+    
     file_prompt =  f"""Based on the summary below, generate a specific and descriptive filename that captures the essence of the document.
     Limit the filename to a maximum of 3 words. Use nouns and avoid starting with verbs like 'depicts', 'shows', 'presents', etc.
     Do not include any data type words like 'text', 'document', 'pdf', etc. Use only letters and connect words with underscores.
@@ -38,30 +47,23 @@ def generate_name_text_api(path,text,bar,client,model):
 
     Examples:
     1. Summary: A research paper on the fundamentals of string theory.
-        Filename: fundamentals_of_string_theory
+       Filename: fundamentals_of_string_theory
 
     2. Summary: An article discussing the effects of climate change on polar bears.
-        Filename: climate_change_polar_bears
+       Filename: climate_change_polar_bears
 
     Now generate the filename.
 
     Output only the filename, without any additional text.
 
     Filename:"""
-    completion_filename=client.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": f"{file_prompt}"
-            }
-        ],
+    inputs=token_text(file_prompt,return_tensors='pt').to(model_text.device)
+    outputs=model_text.generate(
+        **inputs,
+        max_new_tokens=3,
     )
-    filename=completion_filename.choices[0].message.content.strip()
-    filename = re.sub(r'^Filename:\s*', '', filename, flags=re.IGNORECASE).strip()
+    filename=token_text.decode(outputs[0],skip_special_tokens=True).split("Filename:")[-1].strip()
     bar()
-
-    #foldername
     folder_prompt=f"""Using the summary below, identify a broad category or theme that best represents the main topic of the document.
     - This will be used as a folder name.
     - Limit the category to 1 or 2 words.
@@ -76,21 +78,16 @@ def generate_name_text_api(path,text,bar,client,model):
     Now generate the category.
     Return only the category name with no extra text.
     Category:"""
-
-    completion_foldername=client.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": f"{folder_prompt}"
-            }
-        ],
+    inputs=token_text(folder_prompt,return_tensors='pt').to(model_text.device)
+    outputs=model_text.generate(
+        **inputs,
+        max_new_tokens=2,
+        temperature=0.7,
+        top_p=0.85
     )
-    foldername=completion_foldername.choices[0].message.content.strip()
-    foldername=re.sub(r'^Category:\s*','',foldername,flags=re.IGNORECASE).strip()
+    foldername=token_text.decode(outputs[0],skip_special_tokens=True).split("Category")[-1].strip()
     bar()
-
-    #TODO- Fix the commented. 
+    #Todo- fix the commented
     
     # stop_words = set(stopwords.words('english'))
     # lemmatizer = WordNetLemmatizer()
@@ -102,19 +99,30 @@ def generate_name_text_api(path,text,bar,client,model):
         foldername="Untitled"
     return (filename,foldername)
 
-def process_file_text_api(path_text,client,model):
-    path,text=path_text
+
+# In[4]:
+
+
+def process_file_image_local(path,token_image,token_text,model_image,model_text):
+    path=path
     start=time.time()
     with alive_bar(3,title=f"Processing {os.path.basename(path)}") as bar:
-        filename,foldername=generate_name_text_api(path,text,bar,client,model)
+        filename,foldername=generate_name_image_local(path,bar,token_image,token_text,model_image,model_text)
     end=time.time()
 
     print(f"File:{os.path.basename(path)}. Processing done in {end-start:.2f}")
     
     return {"path":path,"file_name":filename,"folder_name":foldername}
 
-def process_files_text_api(path_text_files,client,model):
+def process_files_image_local(path_files,token_image,token_text,model_image,model_text):
     result=[]
-    for path_text in path_text_files:
-        result.append(process_file_text_api(path_text,client,model))
+    for path in path_files:
+        result.append(process_file_image_local(path,token_image,token_text,model_image,model_text))
     return result
+
+
+# In[ ]:
+
+
+
+
